c0c6b41c59f802652c7548a0c4205b8ce2742efc
Drogremtal <bp240@outlook.com>
2021-01-21 08:21:12 +0000

    work

--- Main.java
+++ Main.java
@@ -1,5 +1,43 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+
+
+    String data = GetData("https://swapi.dev/api/");
+    System.out
+        .println(data);
+
   }
+
+    private static String streamToString(InputStream inputStream) {
+    String text = new Scanner(inputStream, "UTF-8").useDelimiter("\\Z").next();
+    return text;
+  }
+
+  public static String GetData (String urlQuery)
+  {
+    String json = "";
+   try {
+      URL url = new URL(urlQuery);
+      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+      connection.setDoOutput(true);
+      connection.setInstanceFollowRedirects(false);
+      connection.setRequestMethod("GET");
+      connection.setRequestProperty("Content-Type", "application/json");
+      connection.setRequestProperty("charset", "utf-8");
+      connection.connect();
+      InputStream inStream = connection.getInputStream();
+      json = streamToString(inStream); // input stream to string
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+	return json;
+  }
+   
 }
